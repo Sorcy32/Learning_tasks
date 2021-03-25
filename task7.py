35d791e1f7d831a44944579b2dead4b20d5d6d90
Sorcerer2005@yandex.ru <LmU9RLSyKaBlyat>
2021-03-25 10:40:44 +0300

    task 7

--- task7.py
+++ task7.py
@@ -1,2 +1,14 @@
 """Написать функцию date, принимающую 3 аргумента — день, месяц и год. Вернуть True, если такая дата есть в нашем
 календаре, и False иначе. """
+from datetime import date as dat
+
+
+def date(dd, mm, yyyy):
+    try:
+        dat(year=yyyy, month=mm, day=dd)
+        return True
+    except ValueError:
+        return False
+
+
+print(date(29, 2, 2020))
